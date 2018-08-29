db32b6b8b2083226f7d506bd8b01aa0865416075
Rui Pereira <ruisantos021@gmail.com>
2018-08-29 22:08:27 +0100

    Scafolded Identity

---
+++ MyWayRazor/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyWayRazor.Data;
+
+[assembly: HostingStartup(typeof(MyWayRazor.Areas.Identity.IdentityHostingStartup))]
+namespace MyWayRazor.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
