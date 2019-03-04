26e1229727d757a33f016ad039fa4a9660e29d1c
Rui Pereira <ruisantos021@gmail.com>
2019-03-04 21:01:47 +0000

    Início das páginas de análise

--- MyWayRazor/Pages/Analise/VooRemoto.cshtml.cs
+++ MyWayRazor/Pages/Analise/VooRemoto.cshtml.cs
@@ -10,11 +10,11 @@ using System.Threading.Tasks;
 
 namespace MyWayRazor.Pages.Analise
 {
-    public class PmrTerminalModel : PageModel
+    public class VooRemotoModel : PageModel
     {
         private readonly ApplicationDbContext db;
 
-        public PmrTerminalModel(ApplicationDbContext context)
+        public VooRemotoModel(ApplicationDbContext context)
         {
             db = context;
         }
@@ -31,9 +31,9 @@ namespace MyWayRazor.Pages.Analise
         public List<string> GateSchengen { get; private set; }
         public List<string> GateNSchengen { get; private set; }
         public List<string> GateT2 { get; private set; }
-        public int TotalPMR { get; private set; }
-        public int TotalPMRDep { get; private set; }
-        public int TotalPMRArr { get; private set; }
+        public int TotalVoos { get; private set; }
+        public int TotalVoosDep { get; private set; }
+        public int TotalVoosArr { get; private set; }
 
         public async Task OnGetAsync()
         {
@@ -49,9 +49,9 @@ namespace MyWayRazor.Pages.Analise
             GateSchengen = GateList(true, true, false).Select(v => v.PortaNum).ToList();
             GateNSchengen = GateList(false, true, false).Select(v => v.PortaNum).ToList();
             GateT2 = GateList(false, false, true).Select(v => v.PortaNum).ToList();
-            TotalPMR = TotaisPmr(true, "");
-            TotalPMRDep = TotaisPmr(false, "D");
-            TotalPMRArr = TotaisPmr(false, "A");
+            TotalVoos = TotaisVoos(true, "");
+            TotalVoosDep = TotaisVoos(false, "D");
+            TotalVoosArr = TotaisVoos(false, "A");
         }
 
         public List<Stand> StandList(int pier, bool remoto)
@@ -95,24 +95,24 @@ namespace MyWayRazor.Pages.Analise
 
         }
 
-        public int TotaisPmr(bool total, string mov)
+        public int TotaisVoos(bool total, string mov)
         {
             if (total == true)
             {
-                int PmrCount = db.AssistenciasPRMS.Where(
+                int VoosCount = db.AssistenciasPRMS.Where(
                     d => d.Data.Date == Hoje
-                    ).Select(v => v.ID).Count();
+                    ).Select(v => v.Voo).Distinct().Count();
 
-                return PmrCount;
+                return VoosCount;
             }
             else
             {
-                int PmrCount = db.AssistenciasPRMS.Where(
+                int VoosCount = db.AssistenciasPRMS.Where(
                     d => d.Data.Date == Hoje
                     && d.Mov == mov
-                    ).Select(v => v.ID).Count();
+                    ).Select(v => v.Voo).Distinct().Count();
 
-                return PmrCount;
+                return VoosCount;
             }
         }
 
@@ -146,5 +146,6 @@ namespace MyWayRazor.Pages.Analise
 
             }
         }
+
     }
 }
